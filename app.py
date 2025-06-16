f08150781d37034550148ca67f0fa9b9a14eca34
HarikaMKHS <harikahaari0914@gmail.com>
2025-06-16 07:16:05 +0530

    checking dashboard

--- app.py
+++ app.py
@@ -418,3 +418,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Creates tables if they don't exist
     app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(debug=True, port=10000)
